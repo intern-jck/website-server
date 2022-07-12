7f9e0b60365fdedc87ce4e869c0a951edcf5259f
Justin C Kirk <justin.c.kirk@gmail.com>
2022-07-12 17:16:20 -0400

    begins server dev

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
+app.use(express.json());
+// const fs = require('fs');
+
 const PORT = 3000;
 
-const projectData = '../data/projectData.json';
+// const projectData = '../data/projectData.json';
 
-app.use(express.json());
 
-app.post('/saveData', (req, res) => {
-  console.log(req);
+app.get('/', (req, res) => {
+  // console.log(req);
   // fs.writeFile(projectData, req.body, (err) => {
   //   if (err) (console.log(err));
   //   console.log('json saved');
   // });
-  res.send(200);
+  // res.sendStatus(req.status);
+  res.send('hello\n');
 });
 
 app.listen(PORT, () => {
